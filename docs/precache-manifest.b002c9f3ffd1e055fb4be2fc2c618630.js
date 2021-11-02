42f4dc638acb9181cd65fd70c2633f1efe072626
Omar Norales <omarnorales@gmail.com>
2021-11-02 11:38:24 -0400

    Folder docs agregado

---
+++ docs/precache-manifest.b002c9f3ffd1e055fb4be2fc2c618630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754f5fb20ec3512ef79480772bbe4b14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bff9b669b04834d736df",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff9b669b04834d736df",
+    "url": "/static/js/main.567fd320.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
